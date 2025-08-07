f2d59a36d6f583943d42761fb4e4f45aaa96b475
bigsuika53 <bigsuika53@gmail.com>
2025-08-07 21:49:45 +0800

    update

---
+++ src/main/java/com/bigsuika/onlineshop/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.bigsuika.onlineshop.repository;
+
+
+import com.bigsuika.onlineshop.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
